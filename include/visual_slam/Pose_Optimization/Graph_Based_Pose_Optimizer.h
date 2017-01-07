bae249ef6feca24e11bb22eb053692a5d82b9054
muhammadaly <muhammadaly92@gmail.com>
2017-01-07 11:16:35 +0200

    add the pose estimation classes

---
+++ include/visual_slam/Pose_Optimization/Graph_Based_Pose_Optimizer.h
@@ -0,0 +1,12 @@
+#ifndef G2OMAPOPTIMIZER_H
+#define G2OMAPOPTIMIZER_H
+
+#include <visual_slam/Pose_Optimization/Pose_Optimizer.h>
+
+class Graph_Based_Pose_Optimizer : public Pose_Optimizer
+{
+public:
+  Graph_Based_Pose_Optimizer();
+};
+
+#endif // G2OMAPOPTIMIZER_H
